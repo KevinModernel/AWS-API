82b59b005f226b4d5923851aac1f5786b0bd3e71
KevinModernel <modernelkevin96@gmail.com>
2023-01-31 20:39:22 -0300

    Clean code
    Fix bugs and reorder functions.

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Scraper = require('@yimura/scraper').default;
 const app = express();
 const youtube = new Scraper()
 
+const apiRecommendationsLink = "https://tastedive.com/api/similar?q=";
+
 // Innitialize Express Server
 const PORT = process.env.PORT || 3000
 try {
@@ -15,56 +17,84 @@ try {
 	console.error('Unable to stablish server: ', e);
 };
 
-app.get('/', async (req, res) => {
-	const playlistLink = await searchArtists('duki');
-	//res.send('<h1>Express con HTML!! 8) </h1>');
-	res.end(JSON.stringify({link: playlistLink}) )
+// Handle GET request.
+app.get('/:input', async (req, res) => {
+	const { input } = req.params
+	// Create playlist link, scrapping youtube video IDs.
+	const playlistLink = await CreatePlayList(input, apiRecommendationsLink);
+
+	// Validate playlistLink.
+	if (playlistLink == false) {
+		res.end(JSON.stringify({link: "Request failed. Try again!"}))		
+	} else {
+		res.end(JSON.stringify({link: playlistLink}))
+	};
 })
 
-
-const api = "https://tastedive.com/api/similar?q=";
-
-//let userInput = 'duki';
-let videosIDs = [];
-
 // Method to search similar artists.
-const searchArtists = async (musicianName) => {
-	let response;
+const CreatePlayList = async (musicianName, apiRecommendationsLink) => {
 
-	try { // HTTP GET Request to get recommendations. ############################ Works Ok.
-		response = await axios.get(`${api}${musicianName}`);
-	} catch (error) {
-		console.log('API tasteDive failed request. Retry.');
+	// HTTP GET Request to get recommendations.
+	const APIRecommendations = await getAPIRecommendations(musicianName, apiRecommendationsLink);
+
+	// Validate API Recommendations response. If False, end process.
+	if (APIRecommendations == false) {
+		return false
 	};
 
-	// Store artists names ############################ Works Ok.
-	let recommendations = [];
-	for(let i=0; i < 4; i++) {
-		recommendations.push(response.data.Similar.Results[i].Name)
-	}
-	console.log(recommendations); // ############################ Works Ok.
+	// Store first 4 recommendations.
+	const recommendations = await storeRecommendations(APIRecommendations);
+	console.log(recommendations);
 
-	// For each recommendation, scrape and store 4 ID videos. // ############################ Works Ok.
-	for (let i=0; i < recommendations.length; i++) {
-		await scrapeIDs(recommendations[i]); // #############Agregaste await aca y ahora si funciona sin settimer
-	};
-
-	// Method to scrape youtube video links
-	async function scrapeIDs (recommendation) {
-		try {
-			//const yt = new youtube.default();
-			let resultados = await youtube.search(recommendation)
-			videosIDs.push(resultados.videos[0].id+','+resultados.videos[1].id+','+resultados.videos[2].id+','+resultados.videos[3].id);
-
-		} catch (error) {
-			console.log(error);	
-		}
-	};
-	console.log(videosIDs);
+	// For each recommendation, get 4 ID videos.
+	const videosIDs = await getVideosIDs(recommendations);
 
 	// Concatenate IDs + youtube base url for playlist.
 	let playlistURL;
 	playlistURL = 'https://www.youtube.com/watch_videos?video_ids='+videosIDs[0]+','+videosIDs[1]+','+videosIDs[2]+','+videosIDs[3];
 	console.log(playlistURL);
-	return playlistURL
+	
+	return playlistURL;
+};
+
+// Method to make HTTP GET Request to API, and get recommendations JSON.
+const getAPIRecommendations = async (musicianName, apiRecommendationsLink) => {
+	try {
+		let response = await axios.get(`${apiRecommendationsLink}${musicianName}`);
+		return response;
+	} catch (error) {
+		console.log('API tasteDive failed request. Retry.');
+		return false
+	}
+};
+// Method to store in an array the name of the first 4 recommendations.
+const storeRecommendations  = (APIRecommendations) => {
+	let recommendations = [];
+	for(let i=0; i < 4; i++) {
+		recommendations.push(APIRecommendations.data.Similar.Results[i].Name)
+	};
+	return recommendations
+};
+// Method to store in an array the videos IDs for each recommendations.
+const getVideosIDs = async (recommendations) => {
+	let videosIDs = [];
+	for (let i=0; i < recommendations.length; i++) {
+		let response = await scrapeIDs(recommendations[i]);
+		videosIDs.push(response)
+	}
+	return videosIDs;
 }
+// Method to scrape 4 youtube videos IDs.
+const scrapeIDs = async (recommendation) => {
+	try {
+		let videosIDs = [];
+		let response = await youtube.search(recommendation);
+		for (let i=0; i < 4; i++) {
+			videosIDs.push(response.videos[i].id);
+		}
+		return videosIDs;
+	} catch (error) {
+		console.log(error);	
+		return
+	}
+};
